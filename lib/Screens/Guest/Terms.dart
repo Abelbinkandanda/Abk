8b999531cf84e2b18db74dd3f2a99648f6cc0de1
abel <ilungakandandaabel@gmail.com>
2022-07-04 20:55:52 +0200

    add terms form

--- lib/Screens/Guest/Terms.dart
+++ lib/Screens/Guest/Terms.dart
@@ -1,18 +1,96 @@
+import 'package:app/main.dart';
 import 'package:flutter/material.dart';
 
-class TermsScreen extends StatefulWidget {
-  TermsScreen({Key? key}) : super(key: key);
+class TermScreen extends StatefulWidget {
+  TermScreen({Key? key}) : super(key: key);
 
   @override
-  State<TermsScreen> createState() => _TermsScreenState();
+  State<TermScreen> createState() => _TermScreenState();
 }
 
-class _TermsScreenState extends State<TermsScreen> {
+class _TermScreenState extends State<TermScreen> {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-        child: Scaffold(
-      appBar: AppBar(),
-    ));
+      child: Scaffold(
+          appBar: AppBar(
+            elevation: 0,
+            titleSpacing: 0.0,
+            backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+            title: Text(
+              'Terms & Conditions',
+              style: TextStyle(color: Colors.black),
+            ),
+            leading: IconButton(
+              icon: Icon(Icons.arrow_back),
+              color: Colors.black,
+              onPressed: () {},
+            ),
+          ),
+          body: Padding(
+              padding: EdgeInsets.only(
+                left: 20.0,
+                right: 20.0,
+                bottom: 15.0,
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Expanded(
+                    child: SingleChildScrollView(
+                      physics: AlwaysScrollableScrollPhysics(),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Text(
+                              "These terms and conditions outline the rules and regulations for the use of Abk Corporation's Website, located at Abk.corp.com.\n"),
+                          Text(
+                              "The following terminology applies to these Terms and Conditions, Privacy Statement and Disclaimer Notice and all Agreements: Client, You and Your refers to you, the person log on this website and compliant to the Company’s terms and conditions.\n "),
+                          Text(
+                              "The Company refers to our Company. Party, or Us, refers to both the Client and ourselves. All terms refer to the offer, acceptance and consideration of payment necessary to undertake the process of our assistance to the Client in the most appropriate manner for the express purpose of meeting the Client’s needs in respect of provision of the Company’s stated services, in accordance with and subject to, prevailing law of Netherlands.\n"),
+                          Text(
+                              "Any use of the above terminology or other words in the singular, plural, capitalization and/or he/she or they, are taken as interchangeable and therefore as referring to same.\n"),
+                          Text(
+                              "By accessing this website we assume you accept these terms and conditions. Do not continue to use Abk if you do not agree to take all of the terms and conditions stated on this page.\n"),
+                          Text(
+                              "The following terminology applies to these Terms and Conditions, Privacy Statement and Disclaimer Notice and all Agreements: Client, You and Your refers to you, the person log on this website and compliant to the Company’s terms and conditions.\n "),
+                          Text(
+                              "The Company refers to our Company. Party, or Us, refers to both the Client and ourselves. All terms refer to the offer, acceptance and consideration of payment necessary to undertake the process of our assistance to the Client in the most appropriate manner for the express purpose of meeting the Client’s needs in respect of provision of the Company’s stated services, in accordance with and subject to, prevailing law of Netherlands.\n"),
+                          Text(
+                              "Any use of the above terminology or other words in the singular, plural, capitalization and/or he/she or they, are taken as interchangeable and therefore as referring to same.\n"),
+                          Text(
+                              "By accessing this website we assume you accept these terms and conditions. Do not continue to use Abk if you do not agree to take all of the terms and conditions stated on this page.\n"),
+                          Text(
+                              "The following terminology applies to these Terms and Conditions, Privacy Statement and Disclaimer Notice and all Agreements: Client, You and Your refers to you, the person log on this website and compliant to the Company’s terms and conditions.\n "),
+                          Text(
+                              "The Company refers to our Company. Party, or Us, refers to both the Client and ourselves. All terms refer to the offer, acceptance and consideration of payment necessary to undertake the process of our assistance to the Client in the most appropriate manner for the express purpose of meeting the Client’s needs in respect of provision of the Company’s stated services, in accordance with and subject to, prevailing law of Netherlands.\n"),
+                          Text(
+                              "Any use of the above terminology or other words in the singular, plural, capitalization and/or he/she or they, are taken as interchangeable and therefore as referring to same.\n"),
+                        ],
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 15.0,
+                  ),
+                  RaisedButton(
+                    padding: EdgeInsets.symmetric(vertical: 15.0),
+                    onPressed: () => print('accept'),
+                    color: Theme.of(context).primaryColor,
+                    elevation: 0,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(0.0),
+                    ),
+                    child: Text(
+                      "accept and continue".toUpperCase(),
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
+                    ),
+                  )
+                ],
+              ))),
+    );
   }
 }
